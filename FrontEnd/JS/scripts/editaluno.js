3db3b24f8bb3934951373a620b249ccaca1df7ea
Lucas Gabriel Amorim Steffen <147677537+LucasGabrielAmorimSteffen@users.noreply.github.com>
2025-02-22 02:56:16 -0400

    feat: alter sessionStorage for editaluno.js

--- FrontEnd/JS/scripts/editaluno.js
+++ FrontEnd/JS/scripts/editaluno.js
@@ -16,3 +16,19 @@ function cancelChanges() {
     // Lógica para cancelar as alterações
     console.log('Alterações canceladas.');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const nomeInput = document.getElementById("nomeA");
+    const cpfInput = document.getElementById("cpfA");
+    const renachInput = document.getElementById("renachA");
+
+    // Pegando os valores armazenados no sessionStorage
+    const nomeSalvo = sessionStorage.getItem("nameStudent");
+    const cpfSalvo = sessionStorage.getItem("CpfStudent");
+    const renachSalvo = sessionStorage.getItem("renachStud");
+
+    // Preenchendo os campos automaticamente
+    if (nomeSalvo) nomeInput.value = nomeSalvo;
+    if (cpfSalvo) cpfInput.value = cpfSalvo;
+    if (renachSalvo) renachInput.value = renachSalvo;
+    });
